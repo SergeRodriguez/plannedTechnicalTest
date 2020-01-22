b79298597e1257436a2c47fea79972e3db17e9c9
SergeRodriguez <serge.rodriguez@mail.mcgill.ca>
2020-01-22 18:01:01 -0500

    Updates

--- precache-manifest.bbb43531f6978cf6a36c1b1a2f39946b.js
+++ precache-manifest.bbb43531f6978cf6a36c1b1a2f39946b.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8f2e440b01e75416f45313c992e46e69",
+    "revision": "3dac50a0eff19b8b0611adf2c9f92054",
     "url": "/plannedTechnicalTest/index.html"
   },
   {
-    "revision": "91905ef04228d8e7bd7f",
+    "revision": "8ba4e466dff43dc3d9eb",
     "url": "/plannedTechnicalTest/static/css/2.968dd0c1.chunk.css"
   },
   {
-    "revision": "43f9a400925f242f9588",
+    "revision": "ef3aec0a390970e86eaa",
     "url": "/plannedTechnicalTest/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "91905ef04228d8e7bd7f",
-    "url": "/plannedTechnicalTest/static/js/2.96d57e44.chunk.js"
+    "revision": "8ba4e466dff43dc3d9eb",
+    "url": "/plannedTechnicalTest/static/js/2.efe35e4c.chunk.js"
   },
   {
     "revision": "092a16da3d0597a43d0dc9ad1e0ddc43",
-    "url": "/plannedTechnicalTest/static/js/2.96d57e44.chunk.js.LICENSE"
+    "url": "/plannedTechnicalTest/static/js/2.efe35e4c.chunk.js.LICENSE"
   },
   {
-    "revision": "43f9a400925f242f9588",
-    "url": "/plannedTechnicalTest/static/js/main.5daa5027.chunk.js"
+    "revision": "ef3aec0a390970e86eaa",
+    "url": "/plannedTechnicalTest/static/js/main.b013a30c.chunk.js"
   },
   {
     "revision": "cd8673126ab12627d1e8",
